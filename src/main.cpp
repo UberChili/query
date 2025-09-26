2e02ecec209c2fed9d3372d26713d138dea43a75
Juan Andres Nuñez <nunezlopez.ja@gmail.com>
2025-09-26 16:02:21 -0600

    Lol

--- src/main.cpp
+++ src/main.cpp
@@ -11,14 +11,25 @@ void prompt() {
 		std::cin >> input;
 		if (input.empty()) {
 			continue;
+		} else {
+			break;
 		}
+
+		// TODO
 		// We would do some kind of processing here
+		// Maybe we just call a function that _parses_
+		// parse(input);
 	} while (true);
 }
 
-int main(void) {
-	std::println("Here I go killing again!");
+void parse(std::string query) {
+	if (query.empty()) {
+		std::println("Empty query");
+		return;
+	}
+}
 
+int main(void) {
 	prompt();
 
 	return 0;
