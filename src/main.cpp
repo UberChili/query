575e7ebe997d6d9f2660d4a65975f889d14cede4
Andrés Núñez <nunezlopez.ja@gmail.com>
2025-09-30 10:52:55 -0600

    Added basic filtering functionality via filter_by_species

--- src/main.cpp
+++ src/main.cpp
@@ -48,35 +48,48 @@ return lines;
 }
 
 struct IrisRow {
-    float sepal_length;
-    float sepal_width;
-    float petal_length;
-    float petal_width;
-    std::string species;
+    std::string sepal_length, sepal_width, petal_length, petal_width, species;
 };
+
+std::vector<std::string> filter_by_species(const std::vector<IrisRow>& rows, const std::string& species) {
+    std::vector<std::string> results;
+
+    for (const auto& row : rows) {
+        if (row.species == species) {
+            std::string line = row.petal_length + "," + row.petal_width + "," + row.sepal_length + "," + row.sepal_width + "," + row.species;
+            results.push_back(line);
+        }
+    }
+    return results;
+}
 
 int main(void) {
     // First clear of screen
     system("clear");
 
-    std::vector<std::string> file = load_file("iris.csv");
-    if (file.empty()) {
-        return 1;
-    }
+    // std::vector<std::string> file = load_file("iris.csv");
+    // if (file.empty()) {
+    //     return 1;
+    // }
 
-    for (const auto& line : file) {
-        println("{}", line);
-    }
+    // for (const auto& line : file) {
+    //     println("{}", line);
+    // }
 
     io::CSVReader<5> in("iris.csv"); 
     in.read_header(io::ignore_no_column, "sepal_length", "sepal_width", "petal_length", "petal_width", "species");
-    float sepal_length, sepal_width, petal_length, petal_width;
-    std::string species;
+    std::string sepal_length, sepal_width, petal_length, petal_width, species;
     std::vector<IrisRow> rows_v;
     while (in.read_row(sepal_length, sepal_width, petal_length, petal_width, species)) {
         rows_v.push_back(IrisRow{sepal_length, sepal_width, petal_length, petal_width, species});
     }
 
+    // Let's start with something simple, printing out all the rows where species == "setosa"
+    auto results = filter_by_species(rows_v, "setosa");
+
+    for (const auto& row : results) {
+        std::println("{}", row);
+    }
 
   // // Testing our tree
   // std::vector<int> nums = {70, 5, 2, 4, 8, 7, 3, 3};
