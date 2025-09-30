c91dfc6d0af169264585e0d120148ed8d052756a
Andrés Núñez <nunezlopez.ja@gmail.com>
2025-09-30 10:31:21 -0600

    Added csv.hpp To read csv file

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,32 @@
-#include "bst.hpp"
+// #include "bst.hpp"
 #include <fstream>
 #include <iostream>
 #include <print>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
+#include <csv.hpp>
+
 std::string prompt() {
-	std::string input;
-	do {
-		std::print("prompt> ");
-		std::getline(std::cin, input);
-		if (input.empty()) {
-			continue;
-		} else {
-			std::print("\t{}\n", input);
-			return input;
-		}
-	} while (true);
+    std::string input;
+    do {
+        std::print("prompt> ");
+        std::getline(std::cin, input);
+        if (input.empty()) {
+            continue;
+        } else {
+            std::print("\t{}\n", input);
+            return input;
+        }
+    } while (true);
 }
 
 void parse(std::string query) {
-	if (query.empty()) {
-		std::println("Empty query");
-		return;
-	}
+    if (query.empty()) {
+        std::println("Empty query");
+        return;
+    }
 }
 
 std::vector<std::string> load_file(std::string filename) {
@@ -35,27 +38,45 @@ std::vector<std::string> load_file(std::string filename) {
   if (!ifs) {
     std::println("Error: Could not open file {}.", filename);
     return {};
-  }
-
-  while(std::getline(ifs, line)) {
-    lines.push_back(line);
-  }
-
-  return lines;
 }
 
+while(std::getline(ifs, line)) {
+    lines.push_back(line);
+}
+
+return lines;
+}
+
+struct IrisRow {
+    float sepal_length;
+    float sepal_width;
+    float petal_length;
+    float petal_width;
+    std::string species;
+};
+
 int main(void) {
-	// First clear of screen
-	system("clear");
+    // First clear of screen
+    system("clear");
 
-  std::vector<std::string> file = load_file("iris.csv");
-  if (file.empty()) {
-    return 1;
-  }
+    std::vector<std::string> file = load_file("iris.csv");
+    if (file.empty()) {
+        return 1;
+    }
 
-  for (const auto& line : file) {
-    println("{}", line);
-  }
+    for (const auto& line : file) {
+        println("{}", line);
+    }
+
+    io::CSVReader<5> in("iris.csv"); 
+    in.read_header(io::ignore_no_column, "sepal_length", "sepal_width", "petal_length", "petal_width", "species");
+    float sepal_length, sepal_width, petal_length, petal_width;
+    std::string species;
+    std::vector<IrisRow> rows_v;
+    while (in.read_row(sepal_length, sepal_width, petal_length, petal_width, species)) {
+        rows_v.push_back(IrisRow{sepal_length, sepal_width, petal_length, petal_width, species});
+    }
+
 
   // // Testing our tree
   // std::vector<int> nums = {70, 5, 2, 4, 8, 7, 3, 3};
@@ -65,10 +86,11 @@ int main(void) {
   // }
   // tree.print_tree();
 
-	// while (true) {
-	// 	std::string in_prompt = prompt();
-	// 	// Process the string, parse to AST
-	// };
+    // while (true) {
+    //  std::string in_prompt = prompt();
+    //  // Process the string, parse to AST
+    // };
 
-	return 0;
+    return 0;
+
 }
